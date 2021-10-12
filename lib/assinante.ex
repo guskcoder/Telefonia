5adc5e36372b75ecb7a8919dc32f5c44d3cb669d
Gustavo Henrique <gustavo@imaginative.company>
2021-10-11 23:33:47 -0300

    Plano Add

--- lib/assinante.ex
+++ lib/assinante.ex
@@ -1,18 +1,20 @@
 defmodule Assinante do
-  defstruct nome: nil, numero: nil, cpf: nil
+  defstruct nome: nil, numero: nil, cpf: nil, plano: nil
 
-  def cadastrar(nome, numero, cpf) do
-    (read() ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf}])
+  @assinantes %{:prepago => "pre.txt", :pospago => "pos.txt"}
+
+  def cadastrar(nome, numero, cpf, plano \\ :prepago) do
+    read(plano) ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano}]
     |> :erlang.term_to_binary()
-    |> write()
+    |> write(plano)
   end
 
-  defp write(lista_assinantes) do
-    File.write("assinantes.txt", lista_assinantes)
+  defp write(lista_assinantes, plano) do
+    File.write(@assinantes[plano], lista_assinantes)
   end
 
-  defp read() do
-    {:ok, assinantes} = File.read("assinantes.txt")
+  defp read(plano) do
+    {:ok, assinantes} = File.read(@assinantes[plano])
 
     assinantes
     |> :erlang.binary_to_term()
--- lib/telefonia.ex
+++ lib/telefonia.ex
@@ -1,5 +1,5 @@
 defmodule Telefonia do
-  def cadastrar_assinante(nome, numero, cpf) do
-    Assinante.cadastrar(nome, numero, cpf)
+  def cadastrar_assinante(nome, numero, cpf, plano) do
+    Assinante.cadastrar(nome, numero, cpf, plano)
   end
 end
