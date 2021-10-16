81b5419aaa1959f2f60378009f7ed43418a8ff8c
Gustavo Henrique <gustavo@imaginative.company>
2021-10-16 00:35:02 -0300

    Assinante Read

--- lib/assinante.ex
+++ lib/assinante.ex
@@ -3,17 +3,28 @@ defmodule Assinante do
 
   @assinantes %{:prepago => "pre.txt", :pospago => "pos.txt"}
 
+  def buscar_assinante(numero) do
+    read(:prepago)
+    |> Enum.find(fn assinante -> assinante.numero == numero end)
+  end
+
   def cadastrar(nome, numero, cpf, plano \\ :prepago) do
-    read(plano) ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano}]
-    |> :erlang.term_to_binary()
-    |> write(plano)
+    case buscar_assinante(numero) do
+      nil ->
+        (read(plano) ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano}])
+        |> :erlang.term_to_binary()
+        |> write(plano)
+
+      _assinate ->
+        {:error, "Assinante com este número já está cadastrado"}
+    end
   end
 
   defp write(lista_assinantes, plano) do
     File.write(@assinantes[plano], lista_assinantes)
   end
 
-  defp read(plano) do
+  def read(plano) do
     {:ok, assinantes} = File.read(@assinantes[plano])
 
     assinantes
