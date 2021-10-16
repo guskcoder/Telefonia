2df0d42909a5339fcba9ffd6f06cba629f1d9784
Gustavo Henrique <gustavo@imaginative.company>
2021-10-15 22:43:13 -0300

    Plano Add 3

---
+++ .elixir_linter/build/lib/telefonia/ebin/telefonia.app
@@ -0,0 +1,6 @@
+{application,telefonia,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"telefonia"},
+              {modules,['Elixir.Assinante','Elixir.Telefonia']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
--- lib/telefonia.ex
+++ lib/telefonia.ex
@@ -1,4 +1,9 @@
 defmodule Telefonia do
+
+  def start do
+    File.write("pre.txt", :erlang.term_to_binary([]))
+    File.write("pos.txt", :erlang.term_to_binary([]))
+  end
   def cadastrar_assinante(nome, numero, cpf, plano) do
     Assinante.cadastrar(nome, numero, cpf, plano)
   end
